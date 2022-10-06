80f02979adb05f2e58adf4bcd4dbd2a9140a6ee3
iscodand <iscodand@outlook.com>
2022-10-06 19:25:38 -0300

    Add: finally end register part jesuss

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -1,12 +1,34 @@
 from django import forms
 from django.contrib.auth.models import User
+from .validation import *
 
 
-class SignUpForm(forms.Model):
-    password = forms.CharField(label="Password", widget = forms.PasswordInput)
+class SignUpForm(forms.ModelForm):
+    password1 = forms.CharField(label="Password", widget = forms.PasswordInput)
     password2 = forms.CharField(label="Repeat Password", widget = forms.PasswordInput)
 
     class Meta():
         model = User
         fields = ['username', 'email']
+        
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        email = self.cleaned_data.get('email')
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        error_list = dict()
+        
+        verify_password(field_name='password1', password1=password1, password2=password2, error_list=error_list)
+        
+        if User.objects.filter(email=email).exists():
+            error_list['email'] = 'Email already registered!'
+        
+        if User.objects.filter(username=username).exists():
+            error_list['email'] = 'Username already registered!'
 
+        if error_list is not None:
+            for error in error_list:
+                error_message = error_list[error]
+                self.add_error(error, error_message)
+                
+        return self.cleaned_data
--- apps/authentication/validation.py
+++ apps/authentication/validation.py
@@ -1,3 +1,10 @@
 def verify_empty_fields(field: str, field_name: str, error_list: dict) -> None:
     if not field.strip():
         error_list[field_name] = f'Field {field_name.capitalize()} is required!'
+        
+def verify_password(field_name, password1, password2, error_list) -> None:
+    if password1 != password2:
+        error_list[field_name] = 'Passwords do not match!'
+
+    if len(password1) < 8:
+        error_list[field_name] = 'Password lenght must be higher than 8 digits!'
