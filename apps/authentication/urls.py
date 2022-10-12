1c582767c8186f4075633ff2d1fb2a10e85dbd65
iscodand <iscodand@outlook.com>
2022-10-12 03:14:40 -0300

    Add: end recover password funcionality

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('logout', logout, name='logout'),
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="authentication/password_reset/password_reset.html"), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="authentication/password_reset/password_reset_sent.html"), name="password_reset_done"),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="authentication/password_reset/password_reset_confirm.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="authentication/password_reset/password_reset_complete.html"), name="password_reset_complete"),
     path('home/', home, name='home'),
 ]
--- apps/authentication/validation.py
+++ apps/authentication/validation.py
@@ -1,11 +1,9 @@
-def verify_empty_fields(field: str, field_name: str, error_list: dict) -> None:
-    if not field.strip():
-        error_list[field_name] = f'Field {field_name.capitalize()} is required!'
-        
-def verify_password(field_name, password1, password2, error_list) -> None:
+def verify_password(field_name: str, password1: str, password2: str, error_list: dict) -> None:
     if password1 != password2:
         error_list[field_name] = 'Passwords do not match!'
-
-    if len(password1.strip()) < 8:
-        error_list[field_name] = 'Password lenght must be higher than 8 digits!'
         
+    if password1 or password2 == None:
+        error_list[field_name] = 'Password lenght must be higher than 8 digits!'
+
+    elif len(password1.strip()) < 8:
+        error_list[field_name] = 'Password lenght must be higher than 8 digits!'
